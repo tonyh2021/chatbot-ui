6269dfcabddd7784fca85a3024cc9314bd574fc5
Tony <tonyhx2021@gmail.com>
2025-07-09 20:05:40 -0400

    remove warnings.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -25,6 +25,10 @@ interface RootLayoutProps {
 }
 
 export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+  ),
+
   applicationName: APP_NAME,
   title: {
     default: APP_DEFAULT_TITLE,
--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 })
 
+const isProd = process.env.NODE_ENV === "production"
+
 const withPWA = require("next-pwa")({
-  dest: "public"
+  dest: "public",
+  disable: !isProd
 })
 
 module.exports = withBundleAnalyzer(
